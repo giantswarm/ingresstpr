a0b99ebec8679ed0ce240df0b7124e28b8fe2ce1
Paweł Kopiczko <kopiczko@gmail.com>
2017-12-08 15:54:56 +0100

    glide: pin operatorkit to release-1.7 (#8)

--- vendor/github.com/giantswarm/operatorkit/informer/spec.go
+++ vendor/github.com/giantswarm/operatorkit/informer/spec.go
@@ -1,8 +1,19 @@
 package informer
 
 import (
+	"context"
+
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 )
+
+type Interface interface {
+	Watch(ctx context.Context) (chan watch.Event, chan watch.Event, chan error)
+}
+
+// WatcherFactory is able to create watchers on demand. It takes a watch
+// endpoint and a ZeroObjectFactory to be able to decode watched events.
+type WatcherFactory func() (watch.Interface, error)
 
 // ZeroObjectFuncs provides zero values of an object and objects' list ready to
 // be decoded. The provided zero values must not be reused by zeroObjectFactory.
